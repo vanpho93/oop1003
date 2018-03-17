09c518bfcd456c0fd8fe9b293bfcff5996ef1b4a
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-17 18:10:38 +0700

    point

--- d.js
+++ d.js
@@ -5,3 +5,16 @@ viet phuong thuc getDistance
 tra ve khoang cach giua diem do va (0, 0);
 
 */
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getDistance() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+
+const p1 = new Point(1, 1);
+console.log(p1.getDistance());
