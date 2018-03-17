0d0b133b151e6395b12b17b5e19ffbc8c85b4dba
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-17 17:39:59 +0700

    object

---
+++ a.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Teo',
+    age: 10,
+    school: {
+        name: 'Khoa Pham'
+    }
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.school.name);
